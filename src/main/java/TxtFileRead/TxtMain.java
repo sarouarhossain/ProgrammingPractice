86a1e3a5e23fc5baf64213f3a1eff6f88c4420ca
sarouar.hossain10@outlook.com <sarouar.hossain10@outlook.com>
2020-08-04 14:33:44 +0900

    aa

--- src/main/java/TxtFileRead/TxtMain.java
+++ src/main/java/TxtFileRead/TxtMain.java
@@ -1,28 +1,93 @@
 package TxtFileRead;
 
-import java.io.BufferedReader;
-import java.io.IOException;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class TxtMain {
-  public static void main(String[] args) {
+  public static final String XLSX_PATH =
+      "C:\\Users\\sarou\\Desktop\\ProgrammingPractice\\ProgrammingPractice\\src\\main\\resources\\testdata.xlsx";
+
+  public static void main(String[] args) throws IOException, InvalidFormatException {
     Path path =
         Paths.get(
             "C:\\Users\\sarou\\Desktop\\ProgrammingPractice\\ProgrammingPractice\\src\\main\\resources\\data.txt");
+
     Charset charset = StandardCharsets.UTF_8;
 
     Map<String, Double> resultMap = getDebitCreditAmount(path, charset);
 
     System.out.println(
         "DebitAmount: " + resultMap.get("debit") + "   CreditAmount: " + resultMap.get("credit"));
+
+    writeToExcelSheet(resultMap.get("debit"), resultMap.get("credit"));
+  }
+
+  public static void writeToExcelSheet(Double debit, Double credit)
+      throws IOException, InvalidFormatException {
+
+    FileInputStream file =
+        new FileInputStream(
+            "C:\\Users\\sarou\\Desktop\\ProgrammingPractice\\ProgrammingPractice\\src\\main\\resources\\testdata.xlsx");
+
+    XSSFWorkbook workbook = new XSSFWorkbook(file);
+    XSSFSheet sheet = workbook.getSheet("0803");
+
+    boolean isDataWritten = false;
+    for (Row row : sheet) {
+      for (Cell cell : row) {
+        if (cell.getCellTypeEnum().equals(CellType.STRING)
+            && cell.getStringCellValue().equals("EPN Fed Credits Presented")) {
+          row.createCell(5).setCellValue(debit);
+          row.createCell(12).setCellValue(credit);
+          isDataWritten = true;
+          break;
+        }
+      }
+
+      if (isDataWritten) break;
+
+      //      Cell cell = row.getCell(0);
+      //      System.out.println(cell.getStringCellValue());
+      //      if (cell.getStringCellValue().equals("EPN Fed Debits Presented")) {
+      //        row.createCell(5).setCellValue(debit);
+      //        row.createCell(12).setCellValue(credit);
+      //        break;
+      //      }
+    }
+
+    //    Cell cell = null;
+    //
+    //    // column: 7, 13, row 7
+    //    cell = sheet.getRow(6).createCell(5);
+    //    cell.setCellValue(debit);
+    //
+    //    cell = sheet.getRow(6).createCell(12);
+    //    cell.setCellValue(credit);
+
+    file.close();
+
+    Date date = new Date();
+    FileOutputStream outputStream =
+        new FileOutputStream(
+            "C:\\Users\\sarou\\Desktop\\ProgrammingPractice\\ProgrammingPractice\\src\\main\\resources\\testdata_update"
+                + date.getTime()
+                + ".xlsx");
+
+    workbook.write(outputStream);
+    outputStream.close();
+    workbook.close();
   }
 
   public static Map<String, Double> getDebitCreditAmount(Path path, Charset charset) {
